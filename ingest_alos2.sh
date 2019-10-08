742088fbe4d7eb2449871f4e750e0b60d8a456aa
shitong01 <stchin@ntu.edu.sg>
2019-10-08 11:42:28 +0800

    fix bugs!

--- ingest_alos2.sh
+++ ingest_alos2.sh
@@ -5,4 +5,4 @@ BASE_PATH=$(cd "${BASE_PATH}"; pwd)
 # export GDAL env variables (adapted from isce.sh)
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 
-${BASE_PATH}/{$1} > {$1}.log 2>&1
+${BASE_PATH}/$1 > $1.log 2>&1
