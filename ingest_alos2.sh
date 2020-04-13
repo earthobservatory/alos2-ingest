f7a18e25174f186531b5091aea1ec2bcf5b33896
shitong01 <stchin@ntu.edu.sg>
2020-04-13 20:01:36 +0800

    update installations with latest docker file

--- ingest_alos2.sh
+++ ingest_alos2.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 BASE_PATH=$(dirname "${BASH_SOURCE}")
 BASE_PATH=$(cd "${BASE_PATH}"; pwd)
+source /opt/isce2/isce_env.sh
 
 # export GDAL env variables (adapted from isce.sh)
 export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
